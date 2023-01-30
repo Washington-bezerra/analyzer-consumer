3b7d91176b5afbcf836da85e34210584084767d9
Washington Bezerra <61431230+Washington-bezerra@users.noreply.github.com>
2023-01-30 00:42:41 -0300

    alter infra folder name

--- src/main/kotlin/com/washington/analyzer/infrastructure/consumer/ItemAnalyzer.kt
+++ src/main/kotlin/com/washington/analyzer/infrastructure/consumer/ItemAnalyzer.kt
@@ -1,8 +1,5 @@
-package com.washington.analyzer.infra.consumer
+package com.washington.analyzer.infrastructure.consumer
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-//import com.washington.analyzer.entities.Item
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import com.analyzer.event.avro.item.Item as ItemAvro
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
@@ -13,13 +10,6 @@ import org.springframework.stereotype.Component
 @Component
 class ItemAnalyzer {
     private val logger = LoggerFactory.getLogger(javaClass)
-
-//    @KafkaListener(topics = ["\${kafka.topics.product}"], groupId = "ppr")
-//    fun listenGroupFoo(consumerRecord: ConsumerRecord<Any, Any>, ack: Acknowledgment) {
-//        logger.info("Message received {}", consumerRecord)
-//
-//        ack.acknowledge()
-//    }
 
     @KafkaListener(topics = ["\${kafka.topics.product}"], groupId = "ppr")
     fun listenGroupFoo(
